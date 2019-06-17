af757f2f0bc3f1de90889b8993e4555e4fef002a
Matthew Houston <mhouston4994@gmail.com>
2019-06-17 01:25:24 +0000

    Index & main sass

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+app.listen(process.env.PORT, process.env.IP, function(req, res){
+    console.log("Server Started");
+})
