1b51cc0714d3503ecc6b11bc19d404f24fc6b5d2
Matthew Houston <mhouston4994@gmail.com>
2019-06-19 02:26:47 +0000

    final

--- app.js
+++ app.js
@@ -6,9 +6,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+//ROUTES
 app.get("/", function(req, res){
-    res.render("home");
+    res.render("home", {title: "My Portfolio"});
 });
+
+app.get("/about", function(req, res){
+    res.render("about", {title: "About Me"});
+});
+
+app.get("/work", function(req, res){
+    res.render("work", {title: "View My Work"});
+});
+
+app.get("/contact", function(req, res){
+    res.render("contact", {title: "Contact Me"});
+});
+
 app.listen(process.env.PORT, process.env.IP, function(req, res){
     console.log("Server Started");
 })
