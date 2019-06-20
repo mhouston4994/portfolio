5f748cdec0f22d62162e5d19a9e012fd0342e281
mhouston4994 <mhouston4994@gmail.com>
2019-06-19 23:09:21 -0600

    updated host

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "../public"));
+app.use(express.static(__dirname + "/public"));
 
 //ROUTES
 app.get("/", function(req, res){
