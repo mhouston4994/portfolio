ac0412ccce4743cf235e71e0cf1e1e9303f538ea
mhouston4994 <mhouston4994@gmail.com>
2019-08-02 18:30:58 -0600

    compressed imgs, added mountain travel to work

--- app.js
+++ app.js
@@ -37,37 +37,6 @@ app.get("/contact", function(req, res){
     res.render("contact", {title: "Contact Me"});
 });
 
-// REQUEST INFO
-   app.post('/contact', function(req, res, next) {
-    async.waterfall([
-      function(done) {
-        var smtpTransport = nodemailer.createTransport({
-          service: 'SendGrid', 
-          auth: {
-            user: 'mhouston4994',
-            pass: process.env.SENDGRIDPW
-          }
-        });
-        var mailOptions = {
-          to: "mhouston4994@gmail.com",
-          from: 'requestForInfo@portfolio.com',
-          subject: 'Request For Info',
-          text: req.body.userText
-        };
-        smtpTransport.sendMail(mailOptions, function(err) {
-          console.log('mail sent');
-          req.flash('success', "Message sent to Matthew! He will connect with you soon!");
-          done(err, 'done');
-        });
-      }
-    ],
-    function(err) {
-      if (err) return next(err);
-      req.flash("error", "Something Went Wrong");
-      res.redirect('/contact');
-    })
-  });
-
 app.listen(listenPort, listenIP, function(req, res){
     console.log("Server Started");
 })
