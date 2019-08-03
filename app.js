84bdd5fb17b121803161d1d0382f0c684d34ad35
mhouston4994 <mhouston4994@gmail.com>
2019-08-02 18:48:18 -0600

    removed unused code for form submit

--- app.js
+++ app.js
@@ -2,23 +2,12 @@ require("dotenv").config();
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var nodemailer = require("nodemailer");
-var session = require('express-session');
-var flash = require("connect-flash");
-var async = require("async");
 var listenPort = process.env.PORT || "3000";
 var listenIP = process.env.IP || "127.0.0.1";
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(session({secret: '{secret}', name: 'session_id', saveUninitialized: false, resave: false}));
-app.use(flash());
-app.use(function(req, res, next){
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
-});
 
 //ROUTES
 app.get("/", function(req, res){
